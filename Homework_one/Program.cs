bb5c8260f9046d01844524940102da2565497c18
yasinagirbas <yasin.agirbas@hotmail.com>
2023-04-04 17:03:24 +0300

    new

--- Homework_one/Program.cs
+++ Homework_one/Program.cs
@@ -1,2 +1,79 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Collections;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //Soru - 1: Klavyeden girilen 20 adet pozitif sayının asal ve asal olmayan olarak 2 ayrı listeye atın. (ArrayList sınıfını kullanara yazınız.)
+
+        //Negatif ve numeric olmayan girişleri engelleyin.
+        //Her bir dizinin elemanlarını büyükten küçüğe olacak şekilde ekrana yazdırın.
+        //Her iki dizinin eleman sayısını ve ortalamasını ekrana yazdırın.
+
+        int n = 20;
+        ArrayList asal = new ArrayList();
+        ArrayList asalDegil = new ArrayList();
+        ArrayList sayilar = new ArrayList();
+
+        int num;
+        try{
+            for(int i=0; i<n; i++) {
+                Console.Write("Lutfen deger giriniz: ");
+                 num = int.Parse(Console.ReadLine());
+                 if (num < 0)
+                    throw new InvalidDataException("Lutfen negatif bir sayi gir.");
+                 else 
+                    sayilar.Add(num);
+
+            }
+        }catch(Exception ex) {
+            Console.WriteLine(ex.Message.ToString());
+        }
+
+        int flag;
+        foreach(var item in sayilar) {
+            flag = 0;
+            int number = Convert.ToInt32(item);
+            if(number == 1)
+                flag = 1;
+            for(int i=2; i< number; i++) {
+                if(number == 2)
+                    break;
+                if(number % i == 0){
+                    flag = 1;
+                    break;
+                }
+            }
+            if (flag == 1)
+                asalDegil.Add(number);
+            else
+                asal.Add(number);
+        }
+
+        asal.Sort();
+        asalDegil.Sort();
+
+        float asalToplam = 0;
+        float asalOlmayanToplam = 0;
+        Console.WriteLine("Asal olanlar: ");
+        foreach(var item in asal){
+            Console.Write(item + ",");
+            asalToplam += Convert.ToInt32(item);
+        }
+
+        Console.WriteLine("\nAsal olmayanlar: ");
+        foreach(var item in asalDegil){
+            Console.Write(item + ",");
+            asalOlmayanToplam += Convert.ToInt32(item);
+        }
+
+        Console.WriteLine("\nAsal olan sayi: " + asal.Count);
+        Console.WriteLine("Asal olan toplam: " + asalToplam);
+        Console.WriteLine("Asal olan ortalama: " + (asalToplam/asal.Count));
+
+        Console.WriteLine("Asal olmayan sayi: " + asalDegil.Count);
+        Console.WriteLine("Asal olmayan toplam: " + asalOlmayanToplam);
+        Console.WriteLine("Asal olmayan ortalama: " + (asalOlmayanToplam/asalDegil.Count));
+
+    }
+}
